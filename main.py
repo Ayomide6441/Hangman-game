2bcfeb7b1998f798c5a384c2adf408059b3337ce
Ayomide6441 <liltush6441@gmail.com>
2022-10-21 13:43:32 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import random
+from level import level, logo
+from words import word_list
+
+# I used my learning from an Udemy course to build this and made some corrections with the course
+# https://www.udemy.com/share/103IHM3@PJN8QF5CPAv_psmvUXUF5IGdf0vFe2chaAoG5Bd8FcOemhteo_JZQKi3zPm6LBqrNw==/
+
+print(logo)
+game_ended = False
+lives = len(level) - 1
+
+random_word = random.choice(word_list)
+word_length = len(random_word)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not game_ended:
+    guess_letter = input("Guess a letter: ").lower()
+
+    if guess_letter in display:
+        print(f"You've already guessed {guess_letter}")
+
+    for position in range(word_length):
+        letter = random_word[position]
+        if letter == guess_letter:
+            display[position] = letter
+    print(f"{' '.join(display)}")
+
+    if guess_letter not in random_word:
+        print(
+            f"You guessed {guess_letter}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            game_ended = True
+            print("You lose, try again")
+
+    if not "_" in display:
+        game_ended = True
+        print("Good job! You win.")
+    print(level[lives])
